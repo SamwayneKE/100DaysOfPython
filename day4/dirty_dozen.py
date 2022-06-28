9c8b29f976824449ecd072458c8cc090599bb80a
Samuel Ndungu <vidsamco@gmail.com>
2022-06-28 23:23:02 +0300

    Updated the file

--- day4/dirty_dozen.py
+++ day4/dirty_dozen.py
@@ -1,1 +1,8 @@
 dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+# List of lists
+dirty_dozen_list_of_lists = [fruits, vegetables]
+print(dirty_dozen_list_of_lists)
