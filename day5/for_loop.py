7337a7ec23b7dcc17fea0abc2e988256fdaa7df0
Samuel Ndungu <vidsamco@gmail.com>
2022-07-04 20:30:28 +0300

    Updated the for_loop.py file

---
+++ day5/for_loop.py
@@ -0,0 +1,11 @@
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit) 
+    print(fruit + " Pie")
+    
+    # Prints the fruits list once. Notice indentation
+    print(fruits)
+
+# Prints the fruits list once. Notice indentation
+print(fruits)
