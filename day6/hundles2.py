09cb6518faac7ea351172f6098647ac0747aa81b
Samuel Ndungu <vidsamco@gmail.com>
2022-07-15 23:08:51 +0300

    Added hundles2.py

---
+++ day6/hundles2.py
@@ -0,0 +1,24 @@
+   
+# This is just code for solving Reeborg's challenge where some functions like  turn_left() is defined. The exercise is done online in the browser at https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    jump()
+
+#     or
+# while at_goal() != True:
+#     jump()    
